c365628c7754f6247e8d29de0e96568a8edfe1d8
Boissady <boissady.mathieu.gmail.com>
2020-09-30 19:47:40 +0200

    initial commit

---
+++ RoomWordSample2/app/src/main/java/com/example/roomwordsample/Database/WordRoomDatabase.kt
@@ -0,0 +1,65 @@
+package com.example.roomwordsample.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.roomwordsample.DAO.WordDao
+import com.example.roomwordsample.Entities.Word
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+
+            // Add sample words.
+            var word = Word("Patate")
+            wordDao.insert(word)
+            word = Word("Carton")
+            wordDao.insert(word)
+
+        }
+    }
+}
---
+++ RoomWordSample2/app/src/main/java/com/example/roomwordsample/WordViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.roomwordsample
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.roomwordsample.Database.WordRoomDatabase
+import com.example.roomwordsample.Entities.Word
+import com.example.roomwordsample.Repository.WordRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+    // Using LiveData and caching what getAlphabetizedWords returns has several benefits:
+    // - We can put an observer on the data (instead of polling for changes) and only update the
+    //   the UI when the data actually changes.
+    // - Repository is completely separated from the UI through the ViewModel.
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application, viewModelScope).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+
+    fun deleteAll() = viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteAll()
+    }
+
+    fun delete(word: String) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(word)
+    }
+}
